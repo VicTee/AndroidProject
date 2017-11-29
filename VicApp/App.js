a4d7fb6ac08a6433713cbfac2e526d97359296d6
Victor Etilo <victoretilo@hotmail.com>
2017-11-29 09:36:49 -0500

    more test on 11_29

--- VicApp/App.js
+++ VicApp/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, } from 'react-native';
 
 export default class App extends React.Component {
   render() {
+	  const=console.log("this is my first app")
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
 		<Text>More test for git</Text>
+	    <Text>This is cool</Text>
       </View>
     );
   }
