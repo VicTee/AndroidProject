5ae1e260b480fa0a7e7664ff099b557c05f6ca90
Victor Etilo <victoretilo@hotmail.com>
2017-11-29 09:40:17 -0500

    Update App.js

--- VicApp/App.js
+++ VicApp/App.js
@@ -10,7 +10,7 @@ export default class App extends React.Component {
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
 		<Text>More test for git</Text>
-	    <Text>This is cool</Text>
+	    <Text style-{color:'black';}>This is cool</Text>
       </View>
     );
   }
